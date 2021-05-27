e312ea3f1fff6d1ce497bd77055a514fdc2f81fd
lkwate <kwatedassiloic@gmail.com>
2021-05-27 16:35:03 +0200

    update the code for docking

--- core/docking/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y autodock-vina
-python3 docking.py --output 'outputs/' --binding_score 'binding_scores/' --config 'configs/' --log 'logs/' --ligand 'ligand-pdbqt/' --protein 'protein-pdbqt/'
